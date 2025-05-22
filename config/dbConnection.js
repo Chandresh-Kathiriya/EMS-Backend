2b724af52cf941d42c4c8606bef9a9a013252ead
Chandresh-Kathiriya <kathiriyachandresh2@gmail.com>
2025-05-23 01:24:24 +0530

    Update dbConnection.js
    abc

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 // config/dbConnection.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, "", {
     timezone: 'Asia/Kolkata',
     host: process.env.DB_HOST,
     dialect: 'mysql',
