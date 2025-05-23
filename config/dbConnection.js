37f63fc6e33eec2d77be9db7b7647335d951b5bf
Chandresh-Kathiriya <kathiriyachandresh2@gmail.com>
2025-05-23 14:38:01 +0530

    verify

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,17 @@
 // config/dbConnection.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, "", {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  '',  // empty string if password is null
+  {
     timezone: 'Asia/Kolkata',
     host: process.env.DB_HOST,
     dialect: 'mysql',
-});
+    port: process.env.DB_PORT || 3306,
+  }
+);
+
 
 module.exports = sequelize;
