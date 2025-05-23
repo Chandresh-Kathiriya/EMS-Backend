e7a0b11ff0eb54e4272374df18f4843425240c1a
Chandresh-Kathiriya <kathiriyachandresh2@gmail.com>
2025-05-23 14:27:58 +0530

    verify

--- middleware/jwtAuthMiddleware.js
+++ middleware/jwtAuthMiddleware.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
-  let token = req.headers['authorization']?.split(' ')[1];
+  // let token = req.headers['authorization']?.split(' ')[1];
 
   // console.log(token)
 
   // if (!token) return res.status(401).json({ message: 'Unauthorized - please log in' });
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.roleId = decoded.roleId
-    req.userId = decoded.id
-    next();
-  } catch (error) {
-    console.log(error, "ERROR")
-    return res.status(401).json({ message: 'Invalid or expired token' });
-  }
+  // try {
+  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  //   req.roleId = decoded.roleId
+  //   req.userId = decoded.id
+  // } catch (error) {
+  //   console.log(error, "ERROR")
+  //   return res.status(401).json({ message: 'Invalid or expired token' });
+  // }
+  next();
 }
 
 module.exports = { verifyToken };
